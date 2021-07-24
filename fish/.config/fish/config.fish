93fe3a3e2131f545aff93f4195d9b6e457fbbac7
Dustin Ryan-Roepsch <dustinroepsch@gmail.com>
2021-07-24 16:08:11 -0700

    [fish] add fish config that alias' ls to exa

---
+++ fish/.config/fish/config.fish
@@ -0,0 +1,4 @@
+if status is-interactive
+    # Commands to run in interactive sessions can go here
+    alias ls="exa --long --color=always --icons --sort type --binary --no-permissions --git"
+end

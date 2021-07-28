04eb9c676ed8b323e9bf2f70973c4ceb19b60eff
Dustin Ryan-Roepsch <dustinroepsch@gmail.com>
2021-07-28 14:18:09 -0700

    [fish] add --all flag to exa alias

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -1,6 +1,6 @@
 if status is-interactive
     # Commands to run in interactive sessions can go here
-    alias ls="exa --long --color=always --icons --sort type --binary --no-permissions --git"
+    alias ls="exa --all --long --color=always --icons --sort type --binary --no-permissions --git"
 
     switch (uname)
         case Darwin

86c605959cb5a3b974155630a530484b9dee948a
Dustin Ryan-Roepsch <dustinroepsch@gmail.com>
2021-07-28 14:15:56 -0700

    [fish] add color and prompt config

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -1,4 +1,9 @@
 if status is-interactive
     # Commands to run in interactive sessions can go here
     alias ls="exa --long --color=always --icons --sort type --binary --no-permissions --git"
+
+    switch (uname)
+        case Darwin
+            fish_add_path ~/.cargo/bin
+    end
 end

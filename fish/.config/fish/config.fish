d939ed6812af4859e3e614802e8d7efff935825d
Dustin Ryan-Roepsch <dustinroepsch@gmail.com>
2021-08-02 11:42:54 -0700

    [fish] add neovide alias that enables multigrid and dissowned

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -1,6 +1,7 @@
 if status is-interactive
     # Commands to run in interactive sessions can go here
     alias ls="exa --all --long --color=always --icons --sort type --binary --no-permissions --git"
+    alias neovide="neovide --multiGrid --disowned"
 
     switch (uname)
         case Darwin

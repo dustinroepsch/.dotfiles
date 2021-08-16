1716e21bf7b0dca8424f276a71428aa613e687b9
Dustin Ryan-Roepsch <dustinroepsch@gmail.com>
2021-08-16 02:41:10 -0700

    [fish] fix multigrid neovide flag

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -1,7 +1,7 @@
 if status is-interactive
     # Commands to run in interactive sessions can go here
     alias ls="exa --all --long --color=always --icons --sort type --binary --no-permissions --git"
-    alias neovide="neovide --multiGrid"
+    alias neovide="neovide --multigrid"
 
     switch (uname)
         case Darwin
